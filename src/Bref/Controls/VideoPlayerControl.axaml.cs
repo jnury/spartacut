726e19b0eb2aee42106ebbb7e6390cb51bbc3824
Julien Nury <julien@nury.ch>
2025-11-04 17:42:15 +0100

    feat: add VideoPlayerControl XAML structure

---
+++ src/Bref/Controls/VideoPlayerControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Bref.Controls;
+
+public partial class VideoPlayerControl : UserControl
+{
+    public VideoPlayerControl()
+    {
+        InitializeComponent();
+    }
+}
